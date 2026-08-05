3605908ba10221b003523ae8711fc8fdd51f63d3
brunnoalmeida23 <brunno_almeida@hotmail.com.br>
2026-08-05 14:47:16 -0300

    Aumenta tamanho do logo e deixa bordas mais arredondadas

--- app/routes/home.py
+++ app/routes/home.py
@@ -49,7 +49,7 @@ async def home_page(request: Request):
     <nav class="navbar navbar-expand-lg bg-jadlog">
         <div class="container">
             <a class="navbar-brand" href="/">
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="/static/img/logo-jadlog.png" alt="JADLOG BRÁS" class="logo-img">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="/static/img/logo-jadlog.png" alt="JADLOG BRÁS" class="logo-img">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
                 <span class="navbar-toggler-icon"></span>
