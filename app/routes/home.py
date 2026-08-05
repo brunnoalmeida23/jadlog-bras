308e70c13db94bee559ea7858dac72f475e8951a
brunnoalmeida23 <brunno_almeida@hotmail.com.br>
2026-08-05 14:36:40 -0300

    Adiciona fundo branco ao logo e ajusta espaçamento

--- app/routes/home.py
+++ app/routes/home.py
@@ -48,7 +48,7 @@ async def home_page(request: Request):
 <body>
     <nav class="navbar navbar-expand-lg bg-jadlog">
         <div class="container">
-            <a class="navbar-brand" href="/">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a class="navbar-brand" href="/">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <img src="/static/img/logo-jadlog.png" alt="JADLOG BRÁS" class="logo-img">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
